c311b3750583c38a9ac1f03829bf1b51ff5a7f39
jobaer <jobaer.du@gmail.com>
2016-02-28 23:33:47 +0600

    Finish set 2 exercises

--- src/Set2.hs
+++ src/Set2.hs
@@ -21,18 +21,20 @@ tailMay (x:xs) = Just xs
 
 lookupMay :: Eq a => a -> [(a, b)] -> Maybe b
 lookupMay _ [] = Nothing 
-lookupMay a ((x,y):xs) = if (a == x) then (Just y) else (lookupMay a xs)
-
-divMay :: (Eq a, Fractional a) => a -> a -> Maybe a
-divMay a b = if (b == 0) then Nothing else Just (a / b)
-
-maximumMay :: Ord a => [a] -> Maybe a
-maximumMay [] = Nothing 
-maximumMay xs = Just (foldl1 max xs)
+lookupMay a ((x,y):xs) | (a == x) = (Just y) 
+					   | otherwise = (lookupMay a xs)
 
 minimumMay :: Ord a => [a] -> Maybe a	
 minimumMay [] = Nothing 
 minimumMay xs = Just (foldl1 min xs)
+
+divMay :: (Eq a, Fractional a) => a -> a -> Maybe a
+divMay a b | (b == 0) = Nothing 
+           | otherwise = Just (a / b)
+
+maximumMay :: Ord a => [a] -> Maybe a
+maximumMay [] = Nothing 
+maximumMay xs = Just (foldl1 max xs)
 
 queryGreek :: GreekData -> String -> Maybe Double
 queryGreek gd k = case (lookupMay k gd) of 
@@ -56,8 +58,46 @@ link Nothing _ = Nothing
 link (Just a) f = f a 
 
 queryGreek2 :: GreekData -> String -> Maybe Double
-queryGreek2 gd k = undefined 
+queryGreek2 gd k = link (lookupMay k gd) (\ls -> 
+	link (headMay ls) (\h -> 
+		link (tailMay ls) (\t -> 
+			link (maximumMay t) (\m -> 
+				divMay (fromIntegral m) (fromIntegral h)
+				)
+			)	
+		)
+	)
+
+addSalaries :: [(String, Integer)] -> String -> String -> Maybe Integer
+addSalaries ss a b = link (lookupMay a ss) (\s1 -> 
+	link (lookupMay b ss) (\s2 -> 
+		mkMaybe (s1 + s2))) 
+
+yLink :: (a -> b -> c) -> Maybe a -> Maybe b -> Maybe c 
+yLink f ma mb = link ma (\a -> link mb (\b -> mkMaybe (f a b))) 
+
+addSalaries2 :: [(String, Integer)] -> String -> String -> Maybe Integer
+addSalaries2 ss a b = yLink (+) (lookupMay a ss) (lookupMay b ss)
 
 
+mkMaybe :: a -> Maybe a
+mkMaybe a = Just a 
 
+tailProd :: Num a => [a] -> Maybe a 
+tailProd ls = transMaybe product (tailMay ls)
 
+tailSum :: Num a => [a] -> Maybe a 
+tailSum ls = transMaybe sum (tailMay ls)
+
+transMaybe :: (a -> b) -> Maybe a -> Maybe b 
+transMaybe f ma = link ma (\a -> mkMaybe (f a)) 
+
+tailMax :: Ord a => [a] -> Maybe a 
+tailMax ls = combine (transMaybe maximumMay (tailMay ls)) 
+
+tailMin :: Ord a => [a] -> Maybe a 
+tailMin ls = combine (transMaybe minimumMay (tailMay ls))
+
+combine :: Maybe (Maybe a) -> Maybe a 
+combine Nothing = Nothing 
+combine (Just x) = x  
