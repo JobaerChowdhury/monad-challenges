64cee150d7f9a4233c76fde0ce5b384f8cec230d
JobaerChowdhury <jobaer.du@gmail.com>
2016-02-29 16:39:54 +0600

    Set3 start

---
+++ src/Set3.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE MonadComprehensions #-}
+{-# LANGUAGE RebindableSyntax  #-}
+
+module Set3 where
+
+import MCPrelude
+
+allPairs :: [a] -> [b] -> [(a,b)]
+allPairs as bs = allCombs (,) as bs
+
+data Card = Card Int String 
+
+instance Show Card where
+	show (Card i s) = (show i) ++ s 
+
+allCards :: [Int] -> [String] -> [Card]
+allCards is ss = allCombs Card is ss 
+
+allCombs :: (a -> b -> c) -> [a] -> [b] -> [c]
+allCombs _ [] _ = []
+allCombs _ _ [] = []
+allCombs f (a:as) bs = (placeOne f a bs) ++ (allCombs f as bs)
+
+placeOne :: (a -> b -> c) -> a -> [b] -> [c]
+placeOne _ _ [] = []
+placeOne f x (y:ys) = [f x y] ++ (placeOne f x ys)
